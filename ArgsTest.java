513e571021ff9289e2be631280b50fd600c41508
jesmarquez <jesmarquezacevedo@yahoo.es>
2015-01-20 23:06:15 -0500

    fix ArgTest

--- ArgsTest.java
+++ ArgsTest.java
@@ -1,7 +1,7 @@
 /*
 * Listing 2.1 The Argstest program
 */
-class ArgsTEst {
+class ArgsTest {
 	public static void main(String args[]){
 		for(int i=0; i < args.length;++i){
 			System.out.println("args["+i+"] = "+args[i]);
